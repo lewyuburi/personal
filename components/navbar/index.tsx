ce5d2618b75e858845b5e1d737469f70fde3d850
Lew Yuburi <lewisyubu@gmail.com>
2021-02-22 18:02:32 -0500

    navbar donde

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,23 +1,55 @@
 import React from 'react';
+import Image from 'next/image'
 import 'twin.macro'
 
-const NavbarComponent = () => {
+interface INavbarComponentProps {
+  title?: string
+  subtitle?: string
+  logo?: string
+}
+
+const NavbarComponent = (props: INavbarComponentProps) => {
   return (
     <nav role="navigation">
-      <div tw="container mx-auto p-4 flex flex-wrap items-center md:flex-nowrap">
+      <div tw="container-xl mx-auto px-4 py-2 flex flex-wrap items-center md:flex-nowrap">
+
         <div tw="mr-4 md:mr-8">
-          <a href="#" rel="home">
-            Lewis Yuburi
-          </a>
+          <div tw="flex items-center gap-4">
+            {props?.logo && (
+              <Image
+                src={props.logo}
+                alt="Picture of the author"
+                width={48}
+                height={48}
+                tw="rounded-full"
+              />
+            )}
+            <div tw="flex flex-col">
+              {props?.title && (
+                <a href="/" rel="home">
+                  <span tw="text-xl	font-bold">
+                    {props?.title}
+                  </span>
+                </a>
+              )}
+              {props?.subtitle && (
+                <span tw="text-xs">
+                  {props?.subtitle}
+                </span>
+              )}
+            </div>
+          </div>
         </div>
+
         <div tw="ml-auto md:hidden">
-          <button tw="flex items-center px-3 py-2 border rounded" type="button">
-            <svg tw="h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+          <button tw="flex items-center p-4 rounded" type="button">
+            <svg tw="h-5 w-5" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
               <title>Menu</title>
               <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
             </svg>
           </button>
         </div>
+
         <div tw="w-full md:w-auto md:flex-grow md:flex md:items-center">
           <ul tw="flex flex-col mt-4 -mx-4 pt-4 border-t md:flex-row md:items-center md:mx-0 md:ml-auto md:mt-0 md:pt-0 md:border-0">
             <li>
@@ -31,6 +63,7 @@ const NavbarComponent = () => {
             </li>
           </ul>
         </div>
+
       </div>
     </nav>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,63 @@
 import React from 'react';
+import { GetStaticProps, InferGetStaticPropsType } from 'next'
 
 import Navbar from '@/components/navbar'
 
-const HomePage = () => {
+interface IProfile {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: boolean;
+  name: string;
+  company: string;
+  blog: string;
+  location: string;
+  email?: any;
+  hireable: boolean;
+  bio: string;
+  twitter_username: string;
+  public_repos: number;
+  public_gists: number;
+  followers: number;
+  following: number;
+  created_at: Date;
+  updated_at: Date;
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const res = await fetch('https://api.github.com/users/lewyuburi')
+  const profile: IProfile = await res.json()
+
+  return {
+    props: {
+      profile
+    },
+  }
+}
+
+const HomePage = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
+  console.log(props)
   return (
     <React.Fragment>
-      <Navbar />
+      <Navbar
+        title={props?.profile?.name}
+        subtitle={props?.profile?.bio}
+        logo={props?.profile?.avatar_url}
+      />
     </React.Fragment>
   );
 };
